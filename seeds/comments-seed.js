bec41b2c06793aa61ac94484fcc9b7d4d24463d2
priti patel <patelpr_03@yahoo.com>
2021-11-12 17:32:48 -0600

    addfiles

---
+++ seeds/comments-seed.js
@@ -0,0 +1,48 @@
+const { Comment } = require('../models');
+
+const commentData = [
+    {
+        user_id: 1,
+        post_id: 5,
+        comment_text: "This is amazing!"
+    },
+    {
+        user_id: 4,
+        post_id: 4,
+        comment_text: "Wow, amazing work!"
+    },
+    {
+        user_id: 1,
+        post_id: 4,
+        comment_text: "Awesome! "
+    },
+    {
+        user_id: 3,
+        post_id: 5,
+        comment_text: " Fantastic!"
+    },
+    {
+        user_id: 3,
+        post_id: 2,
+        comment_text: "This is great news!"
+    },
+    {
+        user_id: 3,
+        post_id: 4,
+        comment_text: " Keep up the good work!"
+    },
+    {
+        user_id: 5,
+        post_id: 3,
+        comment_text: "Very useful !"
+    },
+    {
+        user_id: 2,
+        post_id: 1,
+        comment_text: "Nice !"
+    }
+]
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
---
+++ seeds/post-seeds.js
@@ -0,0 +1,24 @@
+const { Post } = require('../models');
+
+
+const postData = [
+    {
+        title: "   ",
+        post_content: "   ",
+        user_id: 3
+    },
+
+
+
+
+
+
+
+
+
+
+    ]
+
+
+
+module.exports = seedPosts;
